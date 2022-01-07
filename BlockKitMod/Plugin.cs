0c575f6020baf96139be647fe9a6432be1e9bfe6
Dennis Lindner <dennis.lindner@hs-osnabrueck.de>
2022-01-07 13:55:31 +0100

    Add Plugin
    Added Plugin Class

---
+++ BlockKitMod/Plugin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRage.Plugins;
+
+namespace BlockKitMod
+{
+    class Plugin : IPlugin
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init(object gameInstance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
